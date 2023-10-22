fc62dadc795572459bea94c1a8a27a319336035f
Paulo Machado <pauloo.jmf@gmail.com>
2023-10-22 19:07:05 -0300

    refactor: ditch openai implementation

--- src/application/container/index.ts
+++ src/application/container/index.ts
@@ -1,19 +1,8 @@
-import { GPT3Repository } from '../../openai/repository/GPT3Repository'
-import OpenAiClient from '../../openai/client/OpenAiClient'
-import openAIConfig from '../../config/OpenAIConfig'
 import { BigTextEmbed } from '../../discord/embeds/BigTextEmbed'
 import { SingleImageEmbed } from '../../discord/embeds/SingleImageEmbed'
-import DalleRepository from '../../openai/repository/DalleRepository'
 
-const openAiClient = new OpenAiClient(openAIConfig)
-const gptRepository = new GPT3Repository(openAiClient)
-const dalleRepository = new DalleRepository(openAiClient)
 
 const container = {
-  openai: {
-    gptRepository,
-    dalleRepository
-  },
   discord: {
     embeds: {
       askGPTEmbed: new BigTextEmbed(),
--- src/repository/GenerativeTextAIRepository.ts
+++
@@ -1,8 +0,0 @@
-export interface GenerateCompletionResponse {
-  response: string
-  model: string
-}
-
-export default interface GenerativeTextAIRepository {
-  generateCompletion: (question: string) => Promise<GenerateCompletionResponse>
-}
